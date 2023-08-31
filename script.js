2494413209368061236cbf0fb190da86e403acc6
Subh <subhranshukhilar@outlook.com>
2023-08-31 23:55:27 +0530

    fixed the choice buttons

--- script.js
+++ script.js
@@ -8,35 +8,45 @@ let Gameboard = () => {
 
 
 
-function gameControl(){
-    let btnChoice;
-    let choiceBtnX = document.querySelector(".x-choice > button")
-    let choiceBtnO = document.querySelector(".o-choice > button")
-    choiceBtnX.addEventListener("click", () => {players(1).choice})
-    choiceBtnO.addEventListener("click", () => {players(0).choice})
-    console.log(btnChoice)
-    let  players = (btnChoice) => {
-        const choice = () => {if(btnChoice == 1){
-                let boxClick = document.querySelector(".game-box > div")
-                let displayChoice = document.querySelector(".choice-box1")
-                boxClick.addEventListener("click", () => {displayChoice.style.display = "block"})
-                console.log("hello")
-            } else if(btnChoice == 0){
-                let boxClick = document.querySelector(".game-box > div")
-                let displayChoice = document.querySelector(".choice-box2")
-                boxClick.addEventListener("click", () => {displayChoice.style.display = "block"})
-            }
-        }
+const gameControl = () => {
+    let gamePlay = () => {
+        let choiceBtnX = document.querySelector(".x-choice > button")
+        let choiceBtnO = document.querySelector(".o-choice > button")
         
-        return{choice};
+        
+        
+        choiceBtnX.addEventListener("click", () => {
+                    let boxClick = document.querySelectorAll(".game-box > div")
+                    let displayChoice = document.querySelectorAll(".choice-box1")
+                    
+                    for(let i = 0; i <boxClick.length; i++){
+                        boxClick[i].addEventListener("click", () => {displayChoice[i].style.display = "block"})
+                    }
+    
+        })
+        choiceBtnO.addEventListener("click", () => {
+                    let boxClick = document.querySelectorAll(".game-box > div")
+                    let displayChoice = document.querySelectorAll(".choice-box2")
+                    for(let i = 0; i <boxClick.length; i++){
+                        boxClick[i].addEventListener("click", () => {displayChoice[i].style.display = "block"})
+                    }
+        })
+
+
+    }
+    
+    let  players = () => {
+        
     
     }
     
 
-    let player = players(btnChoice);
-    player.choice()
+    return {gamePlay};
+
 }
 
-gameControl()
+let play = gameControl();
+play.gamePlay();
+
 
 
